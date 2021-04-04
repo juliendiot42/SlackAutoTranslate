a1535e4acbae0329d8330356f391fb8f731def2b
Julien Diot <juliendiot42@gmail.com>
2021-04-04 11:35:42 +0900

    add: simple rest api

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
+const app = require("express")();
 const translate = require("./translate.js");
 
-(async () => {
-  let out = await translate(
-    (fromLang = "Japanese"),
-    (toLang = "English"),
-    (msg = "おはよう、元気？"),
-    (verbose = true),
-    (headless = true)
-  );
-  console.log(JSON.stringify(out));
-})();
+const PORT = 8080;
+
+app.get("/translate", (req, res) => {
+  console.log("triggered");
+
+  let from = req.query.from;
+  let to = req.query.to;
+  let msg = req.query.msg;
+
+  (async () => {
+    let out = await translate(
+      (fromLang = from),
+      (toLang = to),
+      (msg = msg),
+      (verbose = true),
+      (headless = true)
+    );
+    res.status(200).send(out); //console.log(JSON.stringify(out));
+  })();
+});
+
+app.listen(PORT, () => {
+  console.log(`API running on http://localhost:${PORT}`);
+});
